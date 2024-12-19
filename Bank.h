f977a805f0c12c71f13e953ebdf1ba2b4e8f6ddd
Vadimotry <vadim.krsk09@gmail.com>
2024-12-19 19:49:52 +0700

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -4,40 +4,41 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <iostream>
 
 #include "Customer.h"
 #include "Account.h"
-
-using namespace std;
 
 class Bank {
 private:
     std::vector<std::shared_ptr<Customer>> customers;
     std::vector<std::shared_ptr<Account>> accounts;
-    const std::string CUSTOMERS_FILE = "data/customers.txt";
-    const std::string ACCOUNTS_FILE = "data/accounts.txt";
+    const std::string CUSTOMERS_FILE = "customers.txt";
+    const std::string ACCOUNTS_FILE = "accounts.txt";
+    int shift = 3;
 public:
     Bank();
-    shared_ptr<Customer> GetCustomer(int index) const;
-    std::vector<std::shared_ptr<Customer>> SearchCustomer(const std::string& name) const;
-    void DeleteCustomer(int index);
-    void DeleteAccount(int index);
-    double CalculateTotalAssets() const;
-    void SortAccountsByBalance();
-    void AddCustomer(const std::string& name, const std::string& id);
+    ~Bank();
+    std::shared_ptr<Customer> GetCustomer(int index) const;
+    void AddCustomer(const std::string& name, const std::string& id, const std::string& password);
     void AddAccount(std::shared_ptr<Account> account);
     std::shared_ptr<Account> GetAccount(int index) const;
     void ShowAccounts() const;
     void ShowCustomers() const;
-    void Transfer(std::shared_ptr<Account> from, std::shared_ptr<Account> to, double amount);
-    void ShowAccountInfo(int index) const;
-    void DepositToAccount(int index, double amount);
-    void WithdrawFromAccount(int index, double amount);
-    void ApplyInterestToSavings();
-    void UpdateOverdraftLimit(int index, double newLimit);
-    void Transfer(int fromIndex, int toIndex, double amount);
+    bool ValidateLogin(const std::string& id, const std::string& password) const;
     void LoadData();
     void SaveData() const;
+    void Transfer(int fromIndex, int toIndex, double amount);
+    void ApplyInterestToAllSavings();
+    void ShowAccountInfo(int index) const;
+    double CalculateTotalAssets() const;
+    void SortAccountsByBalance();
+    void WithdrawFromAccount(int index, double amount);
+    void DeleteCustomer(int index);
+    void DeleteAccount(int index);
+    std::string encryptCaesar(const std::string& text) const;
+    std::string decryptCaesar(const std::string& text) const;
+
 };
 
 bool isValidId(const std::string& id);
